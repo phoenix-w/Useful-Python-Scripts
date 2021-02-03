857d52b83ac774b41e26c2b8f28a5382ff788f72
phoenix-w <phoenix-w@users.noreply.github.com>
2021-02-03 10:23:52 -0500

    Update association-rules.py

--- association-rules.py
+++ association-rules.py
@@ -4,7 +4,9 @@ import pandas as pd
 # calculate confidence {A} -> {B}
 def confidence(A, B):
     """
+    Calculate confidence {A} -> {B}.
     A and B are arrays that only contain boolean values.
+    e.g. A=df["antecedent"], B=df["consequent"]
     """
     support_A = A.mean()
     support_AB = np.logical_and(A, B).mean()
@@ -14,7 +16,9 @@ def confidence(A, B):
 # calculate lift {A} -> {B}
 def lift(A, B):
     """
+    Calculate lift {A} -> {B}.
     A and B are arrays that only contain boolean values.
+    e.g. A=df["antecedent"], B=df["consequent"]
     """
     support_A = A.mean()
     support_B = B.mean()
@@ -22,10 +26,25 @@ def lift(A, B):
     return support_AB / (support_A*support_B)
 
 
+# calculate leverage {A} -> {B}
+def leverage(A, B):
+    """
+    Calculate leverage {A} -> {B}.
+    A and B are arrays that only contain boolean values.
+    e.g. A=df["antecedent"], B=df["consequent"]
+    """
+    support_A = A.mean()
+    support_B = B.mean()
+    support_AB = np.logical_and(A, B).mean()
+    return support_AB - support_A*support_B
+
+
 # calculate conviction {A} -> {B}
 def conviction(A, B):
     """
+    Calculate conviction {A} -> {B}.
     A and B are arrays that only contain boolean values.
+    e.g. A=df["antecedent"], B=df["consequent"]
     """
     support_AB = np.logical_and(A, B).mean()
     support_A = A.mean()
@@ -37,7 +56,9 @@ def conviction(A, B):
 # calculate Zhang's metric {A} -> {B}
 def zhang(A, B):
     """
+    Calculate Zhang's metric {A} -> {B}.
     A and B are arrays that only contain boolean values.
+    e.g. A=df["antecedent"], B=df["consequent"]
     """
     support_A = A.mean()
     support_B = B.mean()
